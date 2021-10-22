32215668f81eeb2d4d7f65ac7d8134220f893142
Anton Sadovnikoff <sadovnikoff@gmail.com>
2021-10-22 14:18:00 +0300

    +add class creation test;

---
+++ enso-psr/Tests/EnsoTest.php
@@ -0,0 +1,31 @@
+<?php
+declare(strict_types=1);
+
+use Enso\Helpers\A;
+use Enso\Enso;
+
+use Dotenv\Dotenv;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * @author Anton Sadovnikoff <sadovnikoff@gmail.com>
+ */
+class EnsoTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+    }
+
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
+    }
+
+    public function testClassCanBeCreated()
+    {
+        $service = new Enso();
+
+        $this->assertInstanceOf(Enso::class, $service);
+    }
+}
