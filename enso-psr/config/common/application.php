3f2abb9856a5b2bd90de207d9fe9e73816b4aa4d
Anton Sadovnikov <sadovnikoff@gmail.com>
2025-11-19 21:56:53 +0300

    +tune work with environments; +request via DI;

--- enso-psr/config/common/application.php
+++ enso-psr/config/common/application.php
@@ -2,9 +2,13 @@
 
 use Enso\Helpers\Runtime;
 use Enso\Relay\EmitterInterface;
+use Enso\Relay\Request;
 use Enso\System\CliEmitter;
+use Enso\System\CliRequest;
 use Enso\System\WebEmitter;
-use Psr\Log\LoggerInterface;
+use Enso\System\WebRequest;
+use Enso\Relay\RequestInterface;
+use Psr\Http\Message\ServerRequestInterface;
 
 return [
     EmitterInterface::class => static function (): EmitterInterface
@@ -15,5 +19,15 @@ return [
         }
 
         return new WebEmitter();
+    },
+    
+    RequestInterface::class => static function (): RequestInterface
+    {
+        if (Runtime::isCLI())
+        {
+            return CliRequest::fromGlobals();
+        }
+
+        return WebRequest::fromGlobals();
     },
 ];
