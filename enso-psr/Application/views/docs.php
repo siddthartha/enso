fc7f755a17de2bdd44df2ecbc3ba83c10c758b7b
Anton Sadovnikov <sadovnikoff@gmail.com>
2026-07-22 20:21:24 +0300

    +fix cv;

--- enso-psr/Application/views/docs.php
+++ enso-psr/Application/views/docs.php
@@ -4,7 +4,7 @@
 <html lang="en">
 <head><title>Docs</title>
     <meta name="color-scheme" content="light dark" />
-    <link rel="stylesheet" href="http://markdowncss.github.io/retro/css/retro.css" type="text/css" />
+    <link rel="stylesheet" href="https://markdowncss.github.io/retro/css/retro.css" type="text/css" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" />
 <!--    <link rel="stylesheet" href="https://sindresorhus.com/github-markdown-css/github-markdown.css" type="text/css" /> -->
     <style>
@@ -35,10 +35,10 @@
 <div class="row around-xs">
     <div class="col-xs-10">
         <div class="markdown-body box"><?= $html; ?></div>
-        <div class="markdown-body box">
-            <h4>Donate my work (USDT / TRC20)</h4>
-            <img alt="USDT/TRC20" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWAQMAAAAGz+OhAAAABlBMVEX///8AAABVwtN+AAABL0lEQVR42uzUMYoDMQwFUIEKX0rga6kIyDCFr2XQpVwI/qLZzO6mjWa7KG7yGnukj+hT/1EGbCUlxupAlE3zqFL3ECKpm21tux3wkB73mA216Os+m4iOm4xItU04Xvvyrp0zUn2IyOvc3rTzHqUH//lfMc0u5JszQkvKZthkg4jhHj3KRqo2d8sJ+XVHxdoc1OYcHXBfVDZtADAPd1/8xIo1jAxRCK8eHGWjtlXb4GzCT19utgzBIDUPBp59qZjmlxiC3f3CkrWdykHswXWjNrfNjLz0RVK3fDRwZAzgUTbDzN+xOPO1yva9K2cICQnX7dxrdmZI+pKy6TkmtdyR1x0ls61qCOHfrVu1BoyeuuQWI7IjSDKVdctjm+FYd9g5o9wRIRx9le1ThfoKAAD//yQvhLTrCA8oAAAAAElFTkSuQmCC" />
-        </div>
+<!--        <div class="markdown-body box">-->
+<!--            <h4>Donate my work (USDT / TRC20)</h4>-->
+<!--            <img alt="USDT/TRC20" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAJYAAACWAQMAAAAGz+OhAAAABlBMVEX///8AAABVwtN+AAABL0lEQVR42uzUMYoDMQwFUIEKX0rga6kIyDCFr2XQpVwI/qLZzO6mjWa7KG7yGnukj+hT/1EGbCUlxupAlE3zqFL3ECKpm21tux3wkB73mA216Os+m4iOm4xItU04Xvvyrp0zUn2IyOvc3rTzHqUH//lfMc0u5JszQkvKZthkg4jhHj3KRqo2d8sJ+XVHxdoc1OYcHXBfVDZtADAPd1/8xIo1jAxRCK8eHGWjtlXb4GzCT19utgzBIDUPBp59qZjmlxiC3f3CkrWdykHswXWjNrfNjLz0RVK3fDRwZAzgUTbDzN+xOPO1yva9K2cICQnX7dxrdmZI+pKy6TkmtdyR1x0ls61qCOHfrVu1BoyeuuQWI7IjSDKVdctjm+FYd9g5o9wRIRx9le1ThfoKAAD//yQvhLTrCA8oAAAAAElFTkSuQmCC" />-->
+<!--        </div>-->
     </div>
 </div>
 <canvas id="canvas"></canvas>
